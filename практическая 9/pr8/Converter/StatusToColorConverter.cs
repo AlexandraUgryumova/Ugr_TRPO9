642b4d64a85e2d4fcec9dce4318c48d595b1ef91
Александра Пупкина (Угрюмова) <124123433+AlexandraUgryumova@users.noreply.github.com>
2023-11-14 00:03:40 +0500

    initial commit

---
+++ практическая 9/pr8/Converter/StatusToColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Media;
+
+namespace pr8.Converter
+{
+    class StatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString() == "Admin" && value != null)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Black);
+            }
+            throw new NotImplementedException();
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ практическая 9/pr8/Pages/AllUsersPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using pr8.Classes;
+
+namespace pr8.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AllUsersPage.xaml
+    /// </summary>
+    public partial class AllUsersPage : Page
+    {
+        public ObservableCollection<User> users = new ObservableCollection<User>();
+        public User selectedUser { get; set; }
+        
+        public AllUsersPage()
+        {
+            InitializeComponent();
+
+            users.Add(new User("Masha","Admin", DateTime.Now));
+            users.Add(new User("Petya", "User", DateTime.Now));
+            users.Add(new User("Vasya", "User", DateTime.Now));
+            DataContext = this;
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if(selectedUser != null)
+            {
+                users.Remove(selectedUser);
+            }
+        }
+    }
+}
